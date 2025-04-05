34c8e0ae5867c91203f5c27242b0af59e6ce34e8
Ulrich Kilian <uk@science-and-more.de>
2025-04-05 11:50:45 +0200

    feat: restructure home page model

--- intmon/home/models.py
+++ intmon/home/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel
+
+from cms_blocks.models import LinkBlock, BodyRichTextBlock, BodyImageBlock  # Import blocks from cms_blocks
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    subheader = models.CharField(max_length=255, blank=True)
+    intro = RichTextField(blank=True)
+    links = StreamField([("link", LinkBlock())], use_json_field=True, blank=True)
+    body_content = StreamField([
+        ("richtext", BodyRichTextBlock()),
+        ("image", BodyImageBlock()),
+    ], use_json_field=True, blank=True)
+    footer = RichTextField(blank=True)  # Footer content
 
-    content_panels = Page.content_panels + ["body"]
+    content_panels = Page.content_panels + [
+        FieldPanel("subheader"),
+        FieldPanel("intro"),
+        FieldPanel("links"),
+        FieldPanel("body_content"),
+        FieldPanel("footer"),
+    ]
+
+
+    template = "home/home_page.html"
